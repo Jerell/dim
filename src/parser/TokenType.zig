16744d22a182cb3f3c854a2f4fdf529af9206c48
Jerell James <jerell1998@gmail.com>
2025-09-06 10:36:22 +0100

    TokenType

---
+++ src/parser/TokenType.zig
@@ -0,0 +1,34 @@
+pub const TokenType = enum {
+    Number,
+    Dot,
+    Comma,
+    Underscore,
+    // units
+    Identifier,
+
+    //operations
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Caret,
+    LParen,
+    RParen,
+
+    // for unit conversion
+    In,
+
+    // comparison
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    Or,
+    And,
+
+    Eof,
+};
